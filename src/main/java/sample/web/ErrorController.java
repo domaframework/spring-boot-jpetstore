26a050de5790da67640739f54847cb575df50477
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2014-05-26 15:56:16 +0900

    404 のエラーを専用のページで表示するようにしました

---
+++ src/main/java/sample/web/ErrorController.java
@@ -0,0 +1,13 @@
+package sample.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+
+    @RequestMapping("/404")
+    public String notFound() {
+        return "404";
+    }
+}
--- src/main/java/sample/web/WebMvcConfiguration.java
+++ src/main/java/sample/web/WebMvcConfiguration.java
@@ -1,7 +1,12 @@
 package sample.web;
 
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import sample.formatter.AmountFormatter;
@@ -16,4 +21,19 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
         registry.addFormatter(new AmountFormatter());
         registry.addFormatter(new LocalDateTimeFormatter());
     }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new ServletContainerCustomizer();
+    }
+
+    protected static class ServletContainerCustomizer implements
+            EmbeddedServletContainerCustomizer {
+
+        @Override
+        public void customize(ConfigurableEmbeddedServletContainer factory) {
+            factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/404"));
+        }
+    }
+
 }
