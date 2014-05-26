10748c4f76ba2015ffc6330e969fe0f89a3ea4f0
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2014-05-27 07:14:00 +0900

    UserDetailsService の設定を簡易化しました

--- src/main/java/sample/web/WebSecurityConfiguration.java
+++ src/main/java/sample/web/WebSecurityConfiguration.java
@@ -5,12 +5,10 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
@@ -55,15 +53,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailsService());
-        provider.setPasswordEncoder(passwordEncoder());
-        auth.authenticationProvider(provider);
+        auth.userDetailsService(accountService).passwordEncoder(
+                passwordEncoder());
     }
 
-    @Override
-    protected UserDetailsService userDetailsService() {
-        // TODO
-        return accountService;
-    }
 }
