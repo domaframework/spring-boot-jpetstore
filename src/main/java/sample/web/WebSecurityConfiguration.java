300f4ccf08f9cdc7c913feb54efff992a5051862
t1000leaf <tomofumi.chiba@gmail.com>
2014-09-13 20:47:35 +0900

    spring boot 1.1.6 対応

--- src/main/java/sample/web/WebSecurityConfiguration.java
+++ src/main/java/sample/web/WebSecurityConfiguration.java
@@ -21,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
@@ -36,15 +37,7 @@ import sample.service.AccountService;
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private AccountService accountService;
-
-    @Autowired
     private DataSource dataSource;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public PersistentTokenRepository persistentTokenRepository() {
@@ -65,11 +58,22 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.rememberMe().tokenRepository(persistentTokenRepository());
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.userDetailsService(accountService).passwordEncoder(
-                passwordEncoder());
-    }
+    @Configuration
+    protected static class AuthenticationConfiguration extends
+            GlobalAuthenticationConfigurerAdapter {
 
+        @Autowired
+        private AccountService accountService;
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(accountService).passwordEncoder(
+                    passwordEncoder());
+        }
+    }
 }
